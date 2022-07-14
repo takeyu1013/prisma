a788185e3aa3fac5da7b5f948f1c6f2b5b690472
takeyu1013 <yuto1013@gmail.com>
2022-07-14 22:14:51 +0900

    🗃️

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const prisma = new PrismaClient();
+  (async () => {
+    const allUsers = await prisma.user.findMany();
+    console.log(allUsers);
+  })()
+    .catch((error) => {
+      throw error;
+    })
+    .finally(async () => {
+      prisma.$disconnect;
+    });
+  res.status(200).json({ name: "John Doe" });
 }
